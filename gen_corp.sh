8aa820fff53c2e2b9c6d8c73d1f3127ed5915a7f
root <sontapaa.jokulainen@gmail.com>
2025-12-22 04:47:41 +0200

    Blah

--- gen_corp.sh
+++ gen_corp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-python3 generate_corpus.py ./corpus_fuzz_inputs/ ~/shader_corpus/
+# python3 generate_corpus.py ./corpus_fuzz_inputs/ ~/shader_corpus/
+
+python3 generate_corpus.py ~/shader_sources/ ~/shader_corpus/
+
