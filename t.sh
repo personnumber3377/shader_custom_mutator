c6d3b635d6868ac140d3bda4c6dd02287074f7e6
root <sontapaa.jokulainen@gmail.com>
2025-12-23 03:36:41 +0200

    Added struct definition generation to mutator

--- t.sh
+++ t.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 mutator.py ./actual_shader.glsl crash_test_output.txt --iters 1 ; ./mutated_to_source.sh
+python3 mutator.py ./actual_shader.glsl crash_test_output.txt --iters 5 ; ./mutated_to_source.sh
