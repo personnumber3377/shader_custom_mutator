328dae8837fb0890f0551d87ff0ac96f944a14c6
root <sontapaa.jokulainen@gmail.com>
2025-12-30 05:39:04 +0200

    Blah

--- tests/ternary.glsl
+++ tests/ternary.glsl
@@ -2,7 +2,7 @@ precision mediump float;
 precision mediump int;
 
 void main() {
-    float a = 0, b = 0, c = 0, d = 0;
+    float a = 0.0, b = 0.0, c = 0.0, d = 0.0;
     float w = (a ? a,b : b,c);
     float z = a ? b ? c : d : w;
 }
