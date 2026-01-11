1c1ce9c144e7fe9b7825f51c85aa4705a38c1c48
root <sontapaa.jokulainen@gmail.com>
2026-01-11 21:08:00 +0200

    Blah

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python3 test.py --mutation-bench ~/angle_shader_corpus/min/
+python3 test.py --mutation-bench /home/oof/current_corpus/corpus/angle_fuzzing_custom/finalmin/ # ~/angle_shader_corpus/min/
 
