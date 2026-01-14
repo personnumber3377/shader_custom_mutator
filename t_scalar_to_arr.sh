86096b6cd27d7f79298bca955421ae613d5b322e
root <sontapaa.jokulainen@gmail.com>
2026-01-15 00:11:53 +0200

    Blah

--- t_scalar_to_arr.sh
+++ t_scalar_to_arr.sh
@@ -3,6 +3,6 @@
 set -e
 
 python3 test.py --roundtrip ./tests_complex/function_returns_array.glsl
-python3 test.py --mutation-bench ./mutation_tests/scalar_to_array_functions/
+# python3 test.py --mutation-bench ./mutation_tests/scalar_to_array_functions/
 
 
