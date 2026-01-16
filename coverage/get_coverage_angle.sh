56ee4b919ced180ddd017e2d24a62ed41157dad2
root <sontapaa.jokulainen@gmail.com>
2026-01-16 23:56:39 +0200

    Blah

--- coverage/get_coverage_angle.sh
+++ coverage/get_coverage_angle.sh
@@ -1,4 +1,7 @@
-mkdir -p profraw
+
+rm /home/oof/chromiumstuff/source/src/out/pdfium_cov/profraw/* || true # Delete the old stuff???
+
+mkdir -p /home/oof/chromiumstuff/source/src/out/pdfium_cov/profraw
 
 # Remember to add /home/oof/llvminstall/LLVM-21.1.0-Linux-X64/bin to path!!!
 
@@ -6,7 +9,7 @@ i=0
 for f in min/*; do
   echo "[*] Running $f"
   i=$((i+1))
-  LLVM_PROFILE_FILE="profraw/run_$i.profraw" \
-    ./angle_translator_fuzzer "$f" \
+  LLVM_PROFILE_FILE="/home/oof/chromiumstuff/source/src/out/pdfium_cov/profraw/run_$i.profraw" \
+    /home/oof/chromiumstuff/source/src/out/pdfium_cov/profraw/angle_translator_fuzzer "$f" \
     -timeout=5 || true
 done
