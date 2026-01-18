5c8f76c13e6d2feafe1ed011cbbb92a470ee5da3
root <sontapaa.jokulainen@gmail.com>
2026-01-18 17:26:54 +0200

    Some minor changes

--- rtrip_huge.sh
+++ rtrip_huge.sh
@@ -2,4 +2,4 @@
 
 # python3 test.py --roundtrip --ignore-invalid 1 /home/oof/angle_shader_corpus/min/
 
-python3 test.py --roundtrip --ignore-invalid 0 /home/oof/angle_shader_corpus/min/
+python3 test.py --roundtrip --ignore-invalid 1 /home/oof/angle_shader_corpus/min/
