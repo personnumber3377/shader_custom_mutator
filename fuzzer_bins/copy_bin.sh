8e0240eed606ecf4d204ca5e07a1dd060e423c9f
root <sontapaa.jokulainen@gmail.com>
2026-01-31 00:07:57 +0200

    Blah

--- fuzzer_bins/copy_bin.sh
+++ fuzzer_bins/copy_bin.sh
@@ -8,5 +8,24 @@ zip -r angle_translator_fuzzer.zip angle_translator_fuzzer
 
 rm angle_translator_fuzzer || true
 
+
+
+rm angle_translator_fuzzer_no_assert.zip
+
+cp /home/oof/chromiumstuff/source/src/out/angle_no_assert/angle_translator_fuzzer ./angle_translator_fuzzer_no_assert
+
+zip -r angle_translator_fuzzer_no_assert.zip angle_translator_fuzzer_no_assert
+
+rm angle_translator_fuzzer_no_assert || true
+
+
 # Now commit changes...
 ca
+
+
+
+
+
+
+
+
