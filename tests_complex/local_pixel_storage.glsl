e944d72db8aaaa991fddbb3ba95e1c7de7ca1f22
root <sontapaa.jokulainen@gmail.com>
2026-01-31 00:01:42 +0200

    Blah

--- tests_complex/local_pixel_storage.glsl
+++ tests_complex/local_pixel_storage.glsl
@@ -1,8 +1,11 @@
 HEADER: frag 3 6
-#version 310 es
+#version 300 es
 #extension GL_ANGLE_shader_pixel_local_storage : require
+precision highp float;
+precision highp int;
 
-layout(binding = 0, rgba8) uniform pixelLocalANGLE pls0;
+
+layout(binding = 0, rgba8) uniform highp pixelLocalANGLE pls0;
 
 void main()
 {
