0fd94d983399ff079c6112233adf0a62f6991c46
root <sontapaa.jokulainen@gmail.com>
2026-01-30 22:00:31 +0200

    Blah

---
+++ tests_complex/local_pixel_storage.glsl
@@ -0,0 +1,11 @@
+HEADER: frag 4 10
+#version 310 es
+#extension GL_ANGLE_shader_pixel_local_storage : require
+
+layout(binding = 0, rgba8) uniform pixelLocalANGLE pls0;
+
+void main()
+{
+    vec4 v = pixelLocalLoadANGLE(pls0);
+    pixelLocalStoreANGLE(pls0, v);
+}
