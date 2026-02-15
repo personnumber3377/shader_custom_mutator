350cc8c80c8d8dc75b2455769d97f262fb381956
root <sontapaa.jokulainen@gmail.com>
2026-02-15 04:27:54 +0200

    Blah

--- directx_stuff/copy.sh
+++ directx_stuff/copy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cp /home/oof/chromiumstuff/source/src/out/canvasfuzz/webgsl_translator_fuzzer ./angle_webgsl_translator
+
+# angle_no_assert
+
+cp /home/oof/chromiumstuff/source/src/out/angle_no_assert/webgsl_translator_fuzzer ./angle_webgsl_translator
 
 # ./dawn_angle_fuzzer
 
